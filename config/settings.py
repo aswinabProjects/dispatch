c1d563a6469ee5dac90a2cfccd1f56a4fb8611e6
ASWIN A B <aswin.ab20@gmail.com>
2026-09-14 12:55:55 +0530

    Update settings.py
    CORS configured with front end service

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,10 @@ ALLOWED_HOSTS = os.getenv(
     "localhost,127.0.0.1"
 ).split(",")
 
+CORS_ALLOWED_ORIGINS = os.getenv(
+    "CORS_ALLOWED_ORIGINS",
+    "http://localhost:5173"
+).split(",")
 
 # Application definition
 
